e89ee96124dc07fdf8a64d627d40c5a815e0a742
rxR4GE <heineke.pe@gmail.com>
2023-08-16 20:57:27 +0200

    First Working Bot with Ping

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Mono;
+
+public class Bot {
+    public static void main(String[] args) {
+        String token = auth.token;
+        DiscordClient client = DiscordClient.create(token);
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+
+                    if (message.getContent().equalsIgnoreCase("!ping")) {
+                        return message.getChannel()
+                                .flatMap(channel -> channel.createMessage("pong!"));
+                    }
+
+                    return Mono.empty();
+
+                }));
+        login.block();
+
+    }
+}
---
+++ src/main/java/org/example/auth.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class auth {
+    protected static String token = "MTE0MTQzNzMwMjI5NjU1MTQ2NQ.GlI_Kj.CLGrf-Q2P5V7BvSukYKoI2QE4s-dKMD0OMWxbs";
+}
